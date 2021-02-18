3ce82598036f5412445e06d55197d5e4a9fd8cf6
Mehrab Zaidi <45766880+syedmehrabzaidi@users.noreply.github.com>
2021-02-19 02:49:23 +0500

    Add files via upload

---
+++ Ai 3A.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb  5 12:13:31 2020
+3A
+@author: 37521
+"""
+"""pair = (5,6)
+print(pair[1])
+# --------------------------------dictionary
+studentIds = {'knuth': 42.0, 'turing': 56.0, 'nash': 92.0 }
+x=studentIds["knuth"]
+print(x)
+
+nums = [1,2,3,4,5,6]
+plusOneNums = [x+1 for x in nums]
+print(plusOneNums)
+                                       
+def Lab3():
+        print('Ai Lab')
+Lab3()
+def fact1(X):
+    total = 1
+    for i in range(1,X+1):
+        total = total * i
+    return total
+print(fact1(5))
+#--------------------------pow funtion
+def pow(X,p):
+    total = 1
+    for i in range(p):
+        total = total * X
+    return total
+print(pow(5,2))
+
+def sin(x):
+    total = 0
+    for i in range(40):
+        total =total + ((pow(-1,i)*pow(x,2*i+1)/fact1(2*i+1)))
+    return total
+print(sin(5))                                            
+
+def myfunction():
+ 
+    pass                          """
+    
+class myclass:
+    x = 5
+y=myclass()
+print(y,x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
